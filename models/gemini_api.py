94ff4ad3cc0a243684c98351312beb0b4ee06696
Shiqiu Yang <ysqbjut@163.com>
2025-07-03 21:49:47 +0800

    feat: 测试Gemini api

---
+++ models/gemini_api.py
@@ -0,0 +1,15 @@
+# setup
+import google.generativeai as genai
+ 
+ 
+genai.configure(api_key='AIzaSyCU7Aw1auX2L5lCRbxdgfOw6KEl8nNVGnQ')  
+ 
+# 查询模型
+for m in genai.list_models():
+    print(m.name)
+    print(m.supported_generation_methods)
+
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+response = model.generate_content("告诉我太阳系中最大行星的相关知识")
+print(response.text)
